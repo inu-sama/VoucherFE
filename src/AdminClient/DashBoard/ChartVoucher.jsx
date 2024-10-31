7ca5dee12fc301ee3cbb73c0164b700107fdb6cd
Zatking <117702923+Zatking@users.noreply.github.com>
2024-10-31 22:11:04 +0700

    a

--- src/AdminClient/DashBoard/ChartVoucher.jsx
+++ src/AdminClient/DashBoard/ChartVoucher.jsx
@@ -144,6 +144,8 @@ const ChartVoucher = () => {
     );
   }
 
+  
+
   return (
     <div>
       <label> Service: </label>
--- src/Header_Footer/Nav.jsx
+++ src/Header_Footer/Nav.jsx
@@ -16,7 +16,7 @@ const Nav = () => {
       <div className="p-4">
         <ul className="flex lg:block items-center">
           <li className="w-full lg:hover:px-5 py-2 hover:bg-[#eaf9e7] hover:text-[#4ca771] text-center lg:text-left rounded-full mb-4">
-            <Link to="" className="route text-lg lg:text-xl font-extrabold">
+            <Link to="ChartVoucher" className="route text-lg lg:text-xl font-extrabold">
               <FontAwesomeIcon icon={faChartLine} /> DashBoard
             </Link>
           </li>
