95533fea696ba4f9a0715c319030346b4c42dc1e
Chikaturin <ninhhoaithanh@gmail.com>
2024-11-01 11:53:38 +0700

    thanh

--- src/Login.jsx
+++ src/Login.jsx
@@ -17,7 +17,7 @@ const Login = () => {
   const URL = "http://34.204.80.177:3000/api";
 
   function handleLoginSSO() {
-    sso.redirectToLogin("http://localhost:2116/");
+    sso.redirectToLogin("https://voucher-fe-two.vercel.app/");
   }
   const handleLogin = async (e) => {
     e.preventDefault();
