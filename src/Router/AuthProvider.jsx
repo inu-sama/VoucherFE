af30705d3224ac069df7c79f54ebc8954bf20a8a
Chikaturin <ninhhoaithanh@gmail.com>
2024-11-01 15:00:31 +0700

    "Updated API URL from 'http://34.204.80.177:3000/api' to 'https://server-voucher.vercel.app/api' in multiple files."

--- src/Router/AuthProvider.jsx
+++ src/Router/AuthProvider.jsx
@@ -9,31 +9,8 @@ const AuthProvider = ({ children }) => {
   const navigate = useNavigate();
   const [searchParams] = useSearchParams();
 
-  function setCookie(name, value, days) {
-    let expires = "";
-    if (days) {
-      const date = new Date();
-      date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
-      expires = "; expires=" + date.toUTCString();
-    }
-    document.cookie = name + "=" + (value || "") + expires + "; path=/";
-  }
-
-  function getCookie(name) {
-    const value = `; ${document.cookie}`;
-    const parts = value.split(`; ${name}=`);
-    if (parts.length === 2) return parts.pop().split(";").shift();
-    return null;
-  }
-
   const token =
-    searchParams.get("Token") ||
-    localStorage.getItem("accessToken") ||
-    getCookie("Token");
-
-  if (token) {
-    setCookie("Token", token, 1);
-  }
+    searchParams.get("Token") || localStorage.getItem("accessToken");
 
   useEffect(() => {
     const checkUserAuth = async () => {
