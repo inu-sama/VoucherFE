a0f01f8b03e4f8418bc121c115b85e5763d5b6f6
Ngô Thanh Tâm <22dh114722@st.huflit.edu.vn>
2024-11-01 11:18:54 +0700

    tam

--- src/Login.jsx
+++ src/Login.jsx
@@ -17,7 +17,7 @@ const Login = () => {
   const URL = "http://34.204.80.177:3000/api";
 
   function handleLoginSSO() {
-    sso.redirectToLogin("https://voucher-fe-liard.vercel.app/");
+    sso.redirectToLogin("https://voucher-fe-test-seven.vercel.app/");
   }
   const handleLogin = async (e) => {
     e.preventDefault();
